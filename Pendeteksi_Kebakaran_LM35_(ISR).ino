d99145bfafc3004fd81b522a757adbf2ef99df7c
Ilham-SR <ilhamsulaimanrizqilah@gmail.com>
2016-12-09 16:13:27 +0800

    Add files via upload

---
+++ Pendeteksi_Kebakaran_LM35_(ISR).ino
@@ -0,0 +1,40 @@
+int red = 5;
+int orange = 4;
+int green = 3;
+int buzz = 2;
+int lm35 = A2;
+int value = 0;
+
+void setup() {
+  Serial.begin(9600);
+	pinMode(lm35, INPUT);
+  pinMode(red, OUTPUT);
+  pinMode(orange, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(buzz, OUTPUT);
+
+}
+
+void loop() {
+  value = analogRead(lm35);
+  if (value >= 100)
+  {
+  tone(buzz, 1000,500);
+  digitalWrite(red, HIGH);
+  digitalWrite(orange, LOW);
+  digitalWrite(green, LOW);
+  }
+  else if (value > 80)
+  {
+  digitalWrite(red, LOW);
+  digitalWrite(orange, HIGH);
+  digitalWrite(green, LOW);
+  }
+  else
+  {
+  digitalWrite(red, LOW);
+  digitalWrite(orange, LOW);
+  digitalWrite(green, HIGH);
+  }
+
+}
